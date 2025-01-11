e9fd89351fc5accdafed1796e1fa996554c963da
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-01-11 10:50:20 +0000

    Criação de modelos.

---
+++ SoliGest/SoliGest.Server/Models/AssistanceRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SoliGest.Server.Models
+{
+    public class AssistanceRequest
+    {
+        public int Id { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime ResolutionDate { get; set; }
+        public SolarPanel SolarPanel { get; set; }
+    }
+}
---
+++ SoliGest/SoliGest.Server/Models/SolarPanel.cs
@@ -0,0 +1,10 @@
+﻿namespace SoliGest.Server.Models
+{
+    public class SolarPanel
+    {
+        public int Id { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+    }
+}
