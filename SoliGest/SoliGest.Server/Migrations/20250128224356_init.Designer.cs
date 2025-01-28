befb07ca26ea6ff2a1f307b7399412d30f0643c7
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-01-28 22:44:33 +0000

    Base de dados criada com sucesso.

--- SoliGest/SoliGest.Server/Migrations/20250128224356_init.Designer.cs
+++ SoliGest/SoliGest.Server/Migrations/20250128224356_init.Designer.cs
@@ -12,7 +12,7 @@ using SoliGest.Server.Data;
 namespace SoliGest.Server.Migrations
 {
     [DbContext(typeof(SoliGestServerContext))]
-    [Migration("20250128223602_init")]
+    [Migration("20250128224356_init")]
     partial class init
     {
         /// <inheritdoc />
--- SoliGest/SoliGest.Server/obj/Debug/net8.0/SoliGest.Server.AssemblyInfo.cs
+++ SoliGest/SoliGest.Server/obj/Debug/net8.0/SoliGest.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoliGest.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2645304a25fbe5d7416bde57ab5eb869a2670a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beeb7f3242c98232142d9b27ba65025b34eb4f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoliGest.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoliGest.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
