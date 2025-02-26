b01ddf7a5990308cf5e0324811b16f69f4fc61e5
AfonsoCoelho <202100258@estudantes.ips.pt>
2025-02-26 12:47:34 +0000

    login page

--- SoliGest/SoliGest.Server/obj/Debug/net8.0/SoliGest.Server.AssemblyInfo.cs
+++ SoliGest/SoliGest.Server/obj/Debug/net8.0/SoliGest.Server.AssemblyInfo.cs
@@ -14,19 +14,9 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoliGest.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream:SoliGest/SoliGest.Server/obj/Debug/net8.0/SoliGest.Server.AssemblyInfo.cs
-<<<<<<< HEAD:SoliGest/SoliGest.Server/obj/Debug/net8.0/SoliGest.Server.AssemblyInfo.cs
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beeb7f3242c98232142d9b27ba65025b34eb4f9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179bc8b0635c595757eca615dadf7add5319a84f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoliGest.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoliGest.Server")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b1aa80d5af4ba1c008dc7729b91f35accf0d3b")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+475f592d313c6a710f5640f90910504523ae3f90")]
->>>>>>> Stashed changes:SoliGest/SoliGest/obj/Debug/net8.0/SoliGest.AssemblyInfo.cs
-[assembly: System.Reflection.AssemblyProductAttribute("SoliGest")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SoliGest")]
->>>>>>> 475f592d313c6a710f5640f90910504523ae3f90:SoliGest/SoliGest/obj/Debug/net8.0/SoliGest.AssemblyInfo.cs
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
