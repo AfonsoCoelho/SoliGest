3567439b5a3a19028c0f91262e16b7af484261e0
AfonsoCoelho <202100258@estudantes.ips.pt>
2025-02-27 13:25:05 +0000

    registar page

--- SoliGest/soligest.client/src/app/app-routing.module.ts
+++ SoliGest/soligest.client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistarComponent } from './registar/registar.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [{ path: '', component: LoginComponent }];
