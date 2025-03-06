99ef9560551663f9553d6fe0ed5fc74b14629e21
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-03-06 00:11:34 +0000

    Junção de todos os frontends & Routing login -> Password recovery aplicado.

--- SoliGest/soligest.client/src/app/app-routing.module.ts
+++ SoliGest/soligest.client/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegistarComponent } from './registar/registar.component';
+import { LoginComponent } from './login/login.component';
+import { PwrecoveryComponent } from './pwrecovery/pwrecovery.component';
+import { ChangepwComponent } from './changepw/changepw.component';
 
-const routes: Routes = [{path:'', component:HomeComponent}];
+const routes: Routes = [
+  { path: '', component: RegistarComponent }, // A raiz agora carrega a página de registar
+  { path: 'registar', component: RegistarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent }, // Corrigido para exibir a página de login corretamente
+  { path: 'pwrecovery', component: PwrecoveryComponent },
+  { path: '**', redirectTo: 'registar' } // Qualquer rota inválida vai para registar
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ SoliGest/soligest.client/src/app/changepw/changepw.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-changepw',
+  standalone: false,
+  
+  templateUrl: './changepw.component.html',
+  styleUrl: './changepw.component.css'
+})
+export class ChangepwComponent {
+
+}
