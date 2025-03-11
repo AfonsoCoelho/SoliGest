69fbfa1da69afbba9fc3e5c7e32c0f91f9e23b4b
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-03-11 22:44:57 +0000

    Logout adicionado
    Clique na foto de perfil da página principal redireciona o utilizador para a página de inicio de sessão caso não tenha sessão iniciada. Caso contrário, a sessão é terminada e é mostrado um alerta nesse sentido.

--- SoliGest/soligest.client/src/app/home/home.component.ts
+++ SoliGest/soligest.client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthorizeService } from '../../api-authorization/authorize.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public isSignedIn: boolean = false;
 
+  constructor(private auth: AuthorizeService, private router: Router) { }
+
+  ngOnInit() {
+    this.auth.onStateChanged().subscribe((state: boolean) => {
+      this.isSignedIn = state;
+    });
+  }
+
+  signOut() {
+    if (this.isSignedIn) {
+      this.auth.signOut();
+      this.router.navigateByUrl('');
+      alert("Adeus!");
+    }
+    else {
+      this.router.navigateByUrl('login');
+    }
+  }
 }
