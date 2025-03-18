26c00f8765caece8090d85dc60b538b1c109ea97
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-03-18 12:19:47 +0000

    Adição de injeção de dependências em UserController.

--- SoliGest/SoliGest.Server/Migrations/20250318120931_init.Designer.cs
+++ SoliGest/SoliGest.Server/Migrations/20250318120931_init.Designer.cs
@@ -12,7 +12,7 @@ using SoliGest.Server.Data;
 namespace SoliGest.Server.Migrations
 {
     [DbContext(typeof(SoliGestServerContext))]
-    [Migration("20250318104504_init")]
+    [Migration("20250318120931_init")]
     partial class init
     {
         /// <inheritdoc />
