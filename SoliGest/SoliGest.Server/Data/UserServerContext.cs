227ebba07972d93da1246a5e46065d25c5682db8
AfonsoCoelho <202100258@estudantes.ips.pt>
2025-03-20 22:03:25 +0000

    user context

---
+++ SoliGest/SoliGest.Server/Data/UserServerContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SoliGest.Server.Models;
+
+namespace SoliGest.Server.Data
+{
+    public class UserContext : IdentityDbContext<User>
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Personalizar nomes das tabelas (opcional)
+            builder.Entity<User>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+        }
+
+        public DbSet<User> Users { get; set; } // Tabela de utilizadores
+    }
+}
