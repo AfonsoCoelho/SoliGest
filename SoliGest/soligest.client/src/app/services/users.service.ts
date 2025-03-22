10340f6706f67969218f924bd911c72faa48c818
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-03-22 16:13:00 +0000

    Criação e implementação serviço Angular Users.

---
+++ SoliGest/soligest.client/src/app/services/users.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>('api/Users');
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>('api/Users/' + id);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>('api/Users', user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>('api/Users/' + user.id, user);
+  }
+
+  deleteUser(id: number): Observable<User> {
+    return this.http.delete<User>('api/Users/' + id);
+  }
+}
+
+export interface User {
+  id: string;
+  name: string;
+  age: number;
+}
