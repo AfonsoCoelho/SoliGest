c8be08d2eca29062dbd0af09846c6dc8634426de
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-03-22 17:30:27 +0000

    Fixed Listar Funcionários. Falta apenas adicionar o Cargo.

--- SoliGest/soligest.client/src/app/funcionario/funcionario.component.ts
+++ SoliGest/soligest.client/src/app/funcionario/funcionario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User, UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-funcionario',
@@ -8,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './funcionario.component.css'
 })
 export class FuncionarioComponent {
+  public users: User[] = [];
+  selectedUser: User | undefined;
+  imagepath: string = "/assets/img/plus-18.png";
 
+  constructor(private service: UsersService) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.service.getUsers().subscribe(
+      (result) => {
+        this.users = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  onSelectUser(user: User) {
+    this.selectedUser = user;
+  }
 }
