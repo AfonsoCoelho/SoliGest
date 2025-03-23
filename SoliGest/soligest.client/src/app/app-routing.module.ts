f29f4005e10218611faf30090451a953fa85e309
AfonsoCoelho <202100258@estudantes.ips.pt>
2025-03-23 15:44:01 +0000

    create
    Pagina de create completa falta integracao com o backend

--- SoliGest/soligest.client/src/app/app-routing.module.ts
+++ SoliGest/soligest.client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { CanActivate } from '@angular/router';
 import { PwrecoveryComponent } from './pwrecovery/pwrecovery.component';
 import { ChangepwComponent } from './changepw/changepw.component';
 import { FuncionarioComponent } from './funcionario/funcionario.component';
+import { FuncionarioCreateComponent } from './funcionario-create/funcionario-create.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, // A raiz agora carrega a página de registar
@@ -18,6 +19,7 @@ const routes: Routes = [
   { path: 'pwrecovery', component: PwrecoveryComponent },
   { path: 'changepw', component: ChangepwComponent },
   { path: 'funcionario', component: FuncionarioComponent },
+  { path: 'funcionario-create', component: FuncionarioCreateComponent },
 
   { path: '**', redirectTo: '' } // Qualquer rota inválida vai para registar
 ];
--- SoliGest/soligest.client/src/app/funcionario-create/funcionario-create.component.ts
+++ SoliGest/soligest.client/src/app/funcionario-create/funcionario-create.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService, User } from '../services/users.service'; // Importe o serviço e a interface User
 
 @Component({
   selector: 'app-funcionario-create',
-  standalone: false,
-  
   templateUrl: './funcionario-create.component.html',
-  styleUrl: './funcionario-create.component.css'
+  styleUrls: ['./funcionario-create.component.css'],
+  standalone: false
 })
 export class FuncionarioCreateComponent {
+  // Objeto para armazenar os dados do formulário
+  user: User = {
+    id: '',
+    name: '',
+    //address1: '',
+    //address2: '',
+    email: '',
+    birthDate: undefined,
+    //password: '',
+    //confirmPassword: '',
+    role: '',
+    phoneNumber: 0 // Adicione outros campos conforme necessário
+  };
 
+  constructor(
+    private usersService: UsersService, // Injete o serviço
+    private router: Router // Injete o Router para navegação
+  ) { }
+
+  // Método chamado ao submeter o formulário
+  /*
+  onSubmit() {
+    if (this.user.password !== this.user.confirmPassword) {
+      alert('As passwords não coincidem!');
+      return;
+    } 
+
+    
+    // Chama o serviço para criar o utilizador
+    this.usersService.createUser(this.user).subscribe(
+      (response) => {
+        console.log('Utilizador criado com sucesso:', response);
+        this.router.navigate(['/funcionarios']); // Redireciona para a lista de utilizadores
+      },
+      (error) => {
+        console.error('Erro ao criar utilizador:', error);
+      }
+    );
+  }
+  */
 }
