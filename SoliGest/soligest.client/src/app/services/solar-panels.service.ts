3de896ecbc0fa85b94e080e111cfed3fd53a744d
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-04-02 14:27:12 +0100

    Criação e implementação serviço solar-panels

---
+++ SoliGest/soligest.client/src/app/services/solar-panels.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SolarPanelsService {
+
+  constructor(private http: HttpClient) { }
+
+  getSolarPanels(): Observable<SolarPanel[]> {
+    return this.http.get<SolarPanel[]>('api/SolarPanels');
+  }
+
+  getSolarPanel(id: number): Observable<SolarPanel> {
+    return this.http.get<SolarPanel>('api/SolarPanels/' + id);
+  }
+
+  createSolarPanel(solarPanel: SolarPanel): Observable<SolarPanel> {
+    return this.http.post<SolarPanel>('api/SolarPanels', solarPanel);
+  }
+
+  updateSolarPanel(id: number, name: string, priority: string, status: string, statusClass: string, latitude: number, longitude: number, description: string, phone: string, email: string): Observable<SolarPanel> {
+    return this.http.put<SolarPanel>('api/SolarPanels/' + id, { id, name, priority, status, statusClass, latitude, longitude, description, phone, email });
+  }
+
+  deleteSolarPanel(id: number): Observable<SolarPanel> {
+    return this.http.delete<SolarPanel>('api/SolarPanels/' + id);
+  }
+}
+
+export interface SolarPanel {
+  id: number;
+  name: string;
+  priority?: string;
+  status: string;
+  statusClass: string;
+  latitude?: number;
+  longitude?: number;
+  description?: string;
+  phone?: string;
+  email?: string;
+}
