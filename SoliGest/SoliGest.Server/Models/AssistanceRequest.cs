5c03683b962c3b96964ba419dd4318d8ade0ab9e
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-04-03 23:25:31 +0100

    Remoção de obrigatoriadade dos campos Description & ResolutionDate no model AssistanceRequest.

--- SoliGest/SoliGest.Server/Models/AssistanceRequest.cs
+++ SoliGest/SoliGest.Server/Models/AssistanceRequest.cs
@@ -10,13 +10,11 @@ namespace SoliGest.Server.Models
         [Display(Name = "Hora de pedido")]
         public string RequestDate { get; set; }
 
-        [Required(ErrorMessage = "É obrigatório indicar uma hora de resolução.")]
         [Display(Name = "Hora de resolução")]
         public string ResolutionDate { get; set; }
 
-        [Required(ErrorMessage = "É obrigatório indicar uma descrição do problema.")]
         [Display(Name = "Descrição da avaria")]
-        public required string Description { get; set; }
+        public string Description { get; set; }
 
         [Required(ErrorMessage = "É obrigatório indicar um painel solar.")]
         [Display(Name = "Painel Solar")]
