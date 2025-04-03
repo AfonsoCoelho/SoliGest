65e2d6dd265a400809bd83c2237c4b01d5d9368c
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-04-03 10:14:49 +0100

    SolarPanelSeeder apenas executado quando a BD está vazia.

--- SoliGest/SoliGest.Server/Data/SolarPanelSeeder.cs
+++ SoliGest/SoliGest.Server/Data/SolarPanelSeeder.cs
@@ -9,55 +9,58 @@ namespace SoliGest.Server.Data
     {
         public static async Task SeedSolarPanelsAsync(SoliGestServerContext context)
         {
-            SolarPanel solarPanel1 = new SolarPanel
+            if (await context.FindAsync<SolarPanel>(1) == null)
             {
-                Name = "Rua D. Afonso Henriques, Lisboa",
-                Priority = "Alta",
-                Status = "Vermelho",
-                StatusClass = "status-red",
-                Latitude = 38.7223,
-                Longitude = -9.1393,
-                Description = "Painel próximo ao centro",
-                PhoneNumber = 8,
-                Email = "contato@empresa.com",
-                Address = ""
-            };
+                SolarPanel solarPanel1 = new SolarPanel
+                {
+                    Name = "Rua D. Afonso Henriques, Lisboa",
+                    Priority = "Alta",
+                    Status = "Vermelho",
+                    StatusClass = "status-red",
+                    Latitude = 38.7223,
+                    Longitude = -9.1393,
+                    Description = "Painel próximo ao centro",
+                    PhoneNumber = 8,
+                    Email = "contato@empresa.com",
+                    Address = ""
+                };
 
-            await context.AddAsync<SolarPanel>(solarPanel1);
+                await context.AddAsync(solarPanel1);
 
-            SolarPanel solarPanel2 = new SolarPanel
-            {
-                Name = "Avenida Principal, Almada",
-                Priority = "Média",
-                Status = "Verde",
-                StatusClass = "status-green",
-                Latitude = 38.6790,
-                Longitude = -9.1569,
-                Description = "Painel na filial sul",
-                PhoneNumber = 8,
-                Email = "suporte@empresa.com",
-                Address = ""
-            };
+                SolarPanel solarPanel2 = new SolarPanel
+                {
+                    Name = "Avenida Principal, Almada",
+                    Priority = "Média",
+                    Status = "Verde",
+                    StatusClass = "status-green",
+                    Latitude = 38.6790,
+                    Longitude = -9.1569,
+                    Description = "Painel na filial sul",
+                    PhoneNumber = 8,
+                    Email = "suporte@empresa.com",
+                    Address = ""
+                };
 
-            await context.AddAsync<SolarPanel>(solarPanel2);
+                await context.AddAsync(solarPanel2);
 
-            SolarPanel solarPanel3 = new SolarPanel
-            {
-                Name = "Avenida Principal, Almada",
-                Priority = "Média",
-                Status = "Verde",
-                StatusClass = "status-green",
-                Latitude = 38.6790,
-                Longitude = -9.1569,
-                Description = "Painel na filial sul",
-                PhoneNumber = 8,
-                Email = "suporte@empresa.com",
-                Address = ""
-            };
+                SolarPanel solarPanel3 = new SolarPanel
+                {
+                    Name = "Avenida Principal, Almada",
+                    Priority = "Média",
+                    Status = "Verde",
+                    StatusClass = "status-green",
+                    Latitude = 38.6790,
+                    Longitude = -9.1569,
+                    Description = "Painel na filial sul",
+                    PhoneNumber = 8,
+                    Email = "suporte@empresa.com",
+                    Address = ""
+                };
 
-            await context.AddAsync<SolarPanel>(solarPanel3);
+                await context.AddAsync(solarPanel3);
 
-            await context.SaveChangesAsync();
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
