056192ed0cca429f1eaba9ef551d3b39c5b6b944
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-04-04 17:03:09 +0100

    Testes mal sucedidos.

---
+++ SoliGest/SoliGestIntegrationTests/UsersControllerIntegrationTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SoliGestIntegrationTests
+{
+    public class UsersControllerIntegrationTests
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public UsersControllerIntegrationTests()
+        {
+            _factory = new WebApplicationFactory<Program>();
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/funcionarios")]
+        [InlineData("/funcionarios-create")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
