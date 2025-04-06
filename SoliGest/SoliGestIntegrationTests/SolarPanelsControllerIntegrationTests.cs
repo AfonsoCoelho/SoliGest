413446e6dd944a53a60dac5426abf2f86916f820
Diogo Poupinha <202200184@estudantes.ips.pt>
2025-04-06 16:03:38 +0100

    Update SolarPanelsControllerIntegrationTests.cs
    Testes todos funcionais.

--- SoliGest/SoliGestIntegrationTests/SolarPanelsControllerIntegrationTests.cs
+++ SoliGest/SoliGestIntegrationTests/SolarPanelsControllerIntegrationTests.cs
@@ -50,5 +50,82 @@ namespace SoliGestIntegrationTests
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
+
+        [Fact]
+        public async Task Delete_EndpointReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+
+            // Act
+            //await client.DeleteAsync("/api/AssistanceRequests/1");
+            var response = await client.DeleteAsync("/api/SolarPanels/2");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal(0, response.Content.Headers.ContentLength);
+        }
+
+        [Fact]
+        public async Task Put_EndpointReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+            var spum = new SolarPanelUpdateModel
+            {
+                Id = 2,
+                Name = "New name",
+                Priority = "New priority",
+                Status = "New Status",
+                StatusClass = "New Status Class",
+                Latitude = 1,
+                Longitude = 1,
+                Description = "New description",
+                PhoneNumber = 9,
+                Email = "new@mail.com",
+                Address = "New address"
+            };
+
+            // Act
+            var content = new StringContent(JsonConvert.SerializeObject(spum), Encoding.UTF8, "application/json");
+
+            var response = await client.PutAsync("/api/SolarPanels/2", content);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task Post_EndpointReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            HttpClient client = _factory.CreateClient();
+            var sp = new SolarPanel
+            {
+                Id = 0,
+                Name = "New name",
+                Priority = "New priority",
+                Status = "New Status",
+                StatusClass = "New Status Class",
+                Latitude = 1,
+                Longitude = 1,
+                Description = "New description",
+                PhoneNumber = 9,
+                Email = "new@mail.com",
+                Address = "New address"
+            };
+
+            // Act
+            var content = new StringContent(JsonConvert.SerializeObject(sp), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("/api/SolarPanels/", content);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
     }
 }
